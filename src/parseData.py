9f81371186159285b1e0e320248a604479c7c735
Breven Hettinger <breven95@comcast.net>
2018-03-12 16:55:02 -0600

    Updated parser and output

--- src/parseData.py
+++ src/parseData.py
@@ -1,5 +1,31 @@
 import os, sys, csv
 
+def parseMonth(month):
+	if(month == "Jan"):
+		return '1';
+	elif(month == "Feb"):
+		return '2';
+	elif(month == "Mar"):
+		return '3';
+	elif(month == "Apr"):
+		return '4';
+	elif(month == "May"):
+		return '5';
+	elif(month == "Jun"):
+		return '6';
+	elif(month == "Jul"):
+		return '7';
+	elif(month == "Aug"):
+		return '8';
+	elif(month == "Sep"):
+		return '9';
+	elif(month == "Oct"):
+		return '10';
+	elif(month == "Nov"):
+		return '11';
+	elif(month == "Dec"):
+		return '12';
+
 with open('outputData.txt', 'w') as output:
 	with open('..\SampleData.txt', 'r') as sampleData:
 		spamreader = csv.reader(sampleData, delimiter=' ')
@@ -11,29 +37,118 @@ with open('outputData.txt', 'w') as output:
 		for line in spamreader:
 			
 			if(line[7] == 'From'):
-				output.write("('" + str(line[0]) + "',") 			# Month	
-				output.write("'" + str(line[2]) + "',")				# Day
-				output.write("'" + str(line[3]) + "',")				# Timestamp
-				output.write("'" + str(line[8]).strip(':') + "',")	# IP
-				output.write("'" + str(line[9]) + "',")				# Run
-				output.write("'" + str(line[11]) + "'),\n")			# Command
+				output.write("('2018-" + parseMonth(str(line[0])) + "-" + str(line[2]) + " " + str(line[3]) + "',") 			# DateTime
+				output.write("'" + str(line[8]).strip(':') + "',")																# IP
+				output.write("'" + str(line[9]) + "',")																			# Run
+				output.write("'" + str(line[11]) + "',")																		# Command
+				
+				if(line[9] == 'exec'):											
+					index = 12
+					concat = ""
+					
+					while(str(line[index]) != ")"):
+						concat += str(line[index]) + " "
+						index += 1
+				
+					output.write("'" + concat.lstrip('(').rstrip(" ") + "',")													# Parameters
+					
+					index += 2		# Increment past 'by'
+					
+					output.write("'" + str(line[index]) + "',")																	# Invoker
+					
+					index += 1		# Increment to UID/EUID
+					
+					output.write("'" + str(line[index]).split(":")[1].split("/")[0] + "',")										# UID
+					output.write("'" + str(line[index]).split(":")[1].split("/")[1] + "',")										# EUID
+					
+					index += 1		# Increment to GID/EGID
+					output.write("'" + str(line[index]).split(":")[1].split("/")[0] + "',")										# GID
+					output.write("'" + str(line[index]).split(":")[1].split("/")[1].rstrip(",") + "',")							# EGID
+					
+					index += 2		# Increment to parent process
+					output.write("'" + str(line[index]) + "',")																	# Parent Process
+					
+					index += 1		# Increment to PUID/PEUID
+					output.write("'" + str(line[index]).split(":")[1].split("/")[0] + "',")										# PUID
+					output.write("'" + str(line[index]).split(":")[1].split("/")[1] + "',")										# PEUID
+					
+					index += 1		# Increment to PGID/PEGID
+					output.write("'" + str(line[index]).split(":")[1].split("/")[0] + "',")										# PGID
+					output.write("'" + str(line[index]).split(":")[1].split("/")[1].rstrip(",") + "'")							# PEGID
+					
+				if(line[9] == "chdir"):
+					output.write("'',")																							# Parameters
+					
+					output.write("'" + str(line[13]) + "',")																	# Invoker
+					output.write("'" + str(line[14]).split(":")[1].split("/")[0] + "',")										# UID
+					output.write("'" + str(line[14]).split(":")[1].split("/")[1] + "',")										# EUID
+					output.write("'" + str(line[15]).split(":")[1].split("/")[0] + "',")										# GID
+					output.write("'" + str(line[15]).split(":")[1].split("/")[1].rstrip(",") + "',")							# EGID
+					output.write("'" + str(line[17]) + "',")																	# Parent Process
+					output.write("'" + str(line[18]).split(":")[1].split("/")[0] + "',")										# PUID
+					output.write("'" + str(line[18]).split(":")[1].split("/")[1] + "',")										# PEUID
+					output.write("'" + str(line[19]).split(":")[1].split("/")[0] + "',")										# PGID
+					output.write("'" + str(line[19]).split(":")[1].split("/")[1].rstrip(",") + "'")							# PEGID
+				
+				output.write("),\n")
 			
 			elif(line[7] == 'exec'):
-				output.write("('" + str(line[0]) + "',") 			# Month
-				output.write("'" + str(line[2]) + "',")				# Day
-				output.write("'" + str(line[3]) + "',")				# Timestamp
-				output.write("'" + '' + "',")						# IP
-				output.write("'" + str(line[7]) + "',")				# Run
-				output.write("'" + str(line[9]) + "'),\n")			# Command
+				output.write("('2018-" + parseMonth(str(line[0])) + "-" + str(line[2]) + " " + str(line[3]) + "',") 			# DateTime
+				output.write("'" + '' + "',")																					# IP
+				output.write("'" + str(line[7]) + "',")																			# Run
+				output.write("'" + str(line[9]) + "',")																			# Command
+				index = 10
+				concat = ""
+					
+				while(str(line[index]) != ")"):																					# Parameters
+					concat += str(line[index]) + " "
+					index += 1
+				
+				output.write("'" + concat.lstrip('(').rstrip(" ") + "',")
+				
+				index += 2		# Increment past 'by'
+				output.write("'" + str(line[index]) + "',")																		# Invoker
+				
+				index += 1		# Increment to UID/EUID
+				output.write("'" + str(line[index]).split(":")[1].split("/")[0] + "',")											# UID
+				output.write("'" + str(line[index]).split(":")[1].split("/")[1] + "',")											# EUID
+				
+				index += 1		# Increment to GID/EGID
+				output.write("'" + str(line[index]).split(":")[1].split("/")[0] + "',")											# GID
+				output.write("'" + str(line[index]).split(":")[1].split("/")[1].rstrip(",") + "',")								# EGID
+				
+				index += 2		# Increment to parent process
+				output.write("'" + str(line[index]) + "',")																		# Parent Process
+				
+				index += 1		# Increment to PUID/PEUID
+				output.write("'" + str(line[index]).split(":")[1].split("/")[0] + "',")											# PUID
+				output.write("'" + str(line[index]).split(":")[1].split("/")[1] + "',")											# PEUID
+				
+				index += 1		# Increment to PGID/PEGID
+				output.write("'" + str(line[index]).split(":")[1].split("/")[0] + "',")											# PGID
+				output.write("'" + str(line[index]).split(":")[1].split("/")[1].rstrip(",") + "'")								# PEGID
+				
+				output.write("),\n")
+				
 			
 			elif(line[7] == 'chdir'):
-				output.write("('" + str(line[0]) + "',") 			# Month
-				output.write("'" + str(line[2]) + "',")				# Day
-				output.write("'" + str(line[3]) + "',")				# Timestamp
-				output.write("'" + '' + "',")						# IP
-				output.write("'" + str(line[7]) + "',")				# Run
-				output.write("'" + str(line[9]) + "'),\n")			# Command
+				output.write("('2018-" + parseMonth(str(line[0])) + "-" + str(line[2]) + " " + str(line[3]) + "',") 			# DateTime
+				output.write("'" + '' + "',")																					# IP
+				output.write("'" + str(line[7]) + "',")																			# Run
+				output.write("'" + str(line[9]) + "',")																			# Command
+				output.write("'',")																								# Parameters
+				output.write("'" + str(line[11]) + "',")																		# Invoker
+				output.write("'" + str(line[12]).split(":")[1].split("/")[0] + "',")											# UID
+				output.write("'" + str(line[12]).split(":")[1].split("/")[1] + "',")											# EUID
+				output.write("'" + str(line[13]).split(":")[1].split("/")[0] + "',")											# GID
+				output.write("'" + str(line[13]).split(":")[1].split("/")[1].rstrip(",") + "',")								# EGID
+				output.write("'" + str(line[15]) + "',")																		# Parent Process
+				output.write("'" + str(line[16]).split(":")[1].split("/")[0] + "',")											# PUID
+				output.write("'" + str(line[16]).split(":")[1].split("/")[1] + "',")											# PEUID
+				output.write("'" + str(line[17]).split(":")[1].split("/")[0] + "',")											# PGID
+				output.write("'" + str(line[17]).split(":")[1].split("/")[1].rstrip(",") + "'")								# PEGID
 				
+				output.write("),\n")
 			else:
 				print 'New statement on line ' + i
 				continue
