9957841c6ebb88fc8fff82895d1dae34229cf610
kylhansen <kylhansen@westmont.edu>
2018-03-13 14:17:44 -0700

    test learn.py for first set of values

--- src/learn.py
+++ src/learn.py
@@ -12,8 +12,9 @@ clf = MNB()
 vec = DV()
 
 #A list to contain all the dictionaries of training data
-training_data_X = list()
-training_data_y = list()
+#training_data_X = list()
+#training_data_y = list()
+
 
 #run predictions and train algorithm on new data
 def new_data(query):
@@ -28,33 +29,51 @@ def new_data(query):
 	#Connect to sql database and read table into a pandas dataframe
 	db_connection = sql.connect(host=host, user=user, passwd=passwd, db=db)
 	df = pd.read_sql(query, con=db_connection, parse_dates=['date'])
-
-	#Fit data to test into an appropriate format
-	new_training_data = df.to_dict(orient='records')	
-	X_test = vec.transform(new_training_data).toarray() #Gives the array needed for testing the new values
-
+    
+    #Add attribute that says whether a row is duplciated anywhere else in the dataframe
+    dfNoDates = df.drop('date')
+    dfNoDates['normal'] = dfNoDates.duplicated(keep=False)
+    anomolies = dfNoDates[dfNoDates.normal == False]
+    normal = dfNoDates[dfNoDates.normal == True]
+    
+    anomolies_y = anomolies['normal']
+    anomolies_X = anomolies.drop('normal')
+    
+    normal_y = normal['normal']
+    normal_X = normal.drop('normal')
+    
+    training_X = pd.concat(anomolies_X, normal_X)
+    training_y = pd.concat(anomolies_y, normal_y)
+    
+    X_train = vec.fit_transform(training_X)
+    y_train = np.asarray(training_y)
+	
+	clf.fit(X_train, y_train)
+    
 	#predict on the new values
 	predictions = clf.predict(X_test)
 	for i in range(len(predictions)):
 		#send message if an anomoly is found
-		if predictions[i] == 0:
+		if predictions[i] == False:
 			msg = 'anomoly detected: ' + df.iloc[[i]]
 
-			feedback = get_feedback(msg) #TODO: implement get_feedback(msg)
-			predictions[i] = feedback
+			#feedback = get_feedback(msg) #TODO: implement get_feedback(msg)
+			print predictions[i]
+            
+            
+    '''
+	#Fit data to test into an appropriate format
+	new_training_data = df.to_dict(orient='records')	
+	X_test = vec.transform(new_training_data).toarray() #Gives the array needed for testing the new values
 
-	#train on the new data
-	training_data_X = training_data_X + new_training_data
-	training_data_y = training_data_y + predictions
-
-	X_train = vec.fit_transform(training_data_X).to_array() #Fits the vector to include the new data within the testing values
-	y_train = np.asarray(training_data_y)#TODO: find a way to get the initial training values
-	
-	clf.fit(X_train, y_train)
-
+    #train on the new data
+    training_data_X = training_data_X + new_training_data
+    training_data_y = training_data_y + predictions
+    '''
+            
 #an initial query to get the starting training_data_X and training_data_y values
-initial_query = "SELECT * FROM " + table + " WHERE date <= '2018-02-28 23:59:59' AND date >= '2018-02-01 00:00:00'"
-
+initial_query = "SELECT * FROM " + table
+new_data(initial_query)
 
 
 
